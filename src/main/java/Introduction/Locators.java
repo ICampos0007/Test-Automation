3510d6b7c2a0ed32c5705dad47ea41c1da420396
Irvin Campos <irvincampos76@gmail.com>
2024-04-29 14:37:39 -0700

    practice

---
+++ src/main/java/Introduction/Locators.java
@@ -0,0 +1,36 @@
+package Introduction;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Locators {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:/Users/irvin/Desktop//selenium-server/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+        driver.findElement(By.name("inputPassword")).sendKeys("hello123");
+        driver.findElement(By.className("signInBtn")).click();
+        System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
+//        driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("john@rsa.com");
+        driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("john@rsa.com");
+        driver.findElement(By.xpath("//input[@type= 'text'][2]")).clear();
+//        last resort to find an element if there is nothing unique
+        driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("john@gmail.com");
+        driver.findElement(By.xpath("//form/input[3]")).sendKeys("823892348");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".reset-pwd-btn")));
+        driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+//        System.out.println(driver.findElement(By.className("infoMsg")).getText());
+        System.out.println(driver.findElement(By.cssSelector("form p")).getText());
+    }
+
+}
